49604f6fb78cceddb5eea56e170c6c2541c24ac2
Sayan Banerjee <sayan112207@gmail.com>
2025-05-21 21:53:00 +0530

    List for an unknown topic

--- app/main.py
+++ app/main.py
@@ -1,96 +1,123 @@
 import socket
 import threading
 
-NO_ERROR = 0
-
-
-class RequestValidationException(Exception):
-    code: int
-    message: str
-
-class ApiVersionInvalidException(RequestValidationException):
-    code = 35
-    message = "UNSUPPORTED_VERSION"
-
-class Request:
-    request_api_key: int
-    request_api_version: int
-    correlation_id: int
-    client_id: str | None
-    tagged_fields: list[str] | None
-
-    def __init__(self,request_api_key: int,request_api_version: int,correlation_id: int,client_id: str,):
-        self.request_api_key = request_api_key
-        self.request_api_version = request_api_version
-        self.correlation_id = correlation_id
-        self.client_id = client_id
-
-    def validate(self):
-        if self.request_api_version not in [0, 1, 2, 3, 4]:
-            raise ApiVersionInvalidException
-        
-def parse_request_length(header: bytes) -> int:
-    return int.from_bytes(header, byteorder="big", signed=True)
-
-def parse_request(request: bytes) -> Request:
-    request_api_key = int.from_bytes(request[:2], byteorder="big", signed=True)
-    request_api_version = int.from_bytes(request[2:4], byteorder="big", signed=True)
-    correlation_id = int.from_bytes(request[4:8], byteorder="big", signed=True)
-    client_id = bytes.decode(request[8:], "utf-8")
-    print("request_api_key, request_api_version, correlation_id, client_id ====== ")
-    print(request_api_key, request_api_version, correlation_id, client_id)
-    return Request(request_api_key, request_api_version, correlation_id, client_id)
-
-def create_response(request: Request) -> bytes:
-    message_bytes = request.correlation_id.to_bytes(4, byteorder="big", signed=True)
-    min_version, max_version = 0, 4
-    throttle_time_ms = 0
+def response_api_key_75(id, cursor, array_length, length, topic_name):
     tag_buffer = b"\x00"
-    try:
-        request.validate()
-        error_bytes = NO_ERROR.to_bytes(2, byteorder="big", signed=True)
-    except RequestValidationException as ex:
-        error_bytes = ex.code.to_bytes(2, byteorder="big", signed=True)
-    message_bytes += error_bytes
-    message_bytes += int(3).to_bytes(1, byteorder="big", signed=True)
-    # if request.request_api_key == 18:
-    message_bytes += request.request_api_key.to_bytes(2, byteorder="big", signed=True)
-    message_bytes += min_version.to_bytes(2, byteorder="big", signed=True)
-    message_bytes += max_version.to_bytes(2, byteorder="big", signed=True)
-    message_bytes += tag_buffer
-    # elif request.request_api_key == 75:
-    message_bytes += int(75).to_bytes(2, byteorder="big", signed=True)
-    message_bytes += int(0).to_bytes(2, byteorder="big", signed=True)
-    message_bytes += int(0).to_bytes(2, byteorder="big", signed=True)
-    message_bytes += tag_buffer
-    message_bytes += throttle_time_ms.to_bytes(4, byteorder="big", signed=True)
-    message_bytes += tag_buffer
-    req_len = len(message_bytes).to_bytes(4, byteorder="big", signed=True)
-    response = req_len + message_bytes
-    return response
+    response_header = id.to_bytes(4, byteorder="big") + tag_buffer
+    error_code = int(3).to_bytes(2, byteorder="big")
+    throttle_time_ms = int(0).to_bytes(4, byteorder="big")
+    is_internal = int(0).to_bytes(1, byteorder="big")
+    topic_authorized_operations = b"\x00\x00\x0d\xf8"
+    topic_id = int(0).to_bytes(16, byteorder="big")
+    partition_array = b"\x01"
 
-def handler(client_conn, addr):
-    while True:
-        message_len = parse_request_length(client_conn.recv(4))
-        print(f"message_len {message_len}")
-        request_bytes = client_conn.recv(message_len + 8)
-        print(f"request_bytes are {request_bytes}")
-        print(int.from_bytes(request_bytes, byteorder="big", signed=True))
-        # create response
-        response = create_response(parse_request(request_bytes))
-        # send response
-        client_conn.sendall(response)
-        
-def main():
-    with socket.create_server(("localhost", 9092), reuse_port=True) as server:
-        # client_conn, addr = server.accept()
+    response_body = (
+        throttle_time_ms
+        + int(array_length).to_bytes(1, byteorder="big")
+        + error_code
+        + int(length).to_bytes(1, byteorder="big")
+        + topic_name
+        + topic_id
+        + is_internal
+        + partition_array
+        + topic_authorized_operations
+        + tag_buffer
+        + cursor
+        + tag_buffer
+    )
+
+    total_len = len(response_header) + len(response_body)
+    return int(total_len).to_bytes(4, byteorder="big") + response_header + response_body
+
+def create_msg(id, api_key: int, error_code: int = 0):
+    response_header = id.to_bytes(4, byteorder="big")
+    err = error_code.to_bytes(2, byteorder="big")
+    api_key_bytes = api_key.to_bytes(2, byteorder="big")
+    min_version_api_18, max_version_api_18, min_version_api_75, max_version_api_75 = (
+        0,
+        4,
+        0,
+        0,
+    )
+    tag_buffer = b"\x00"
+    num_api_keys = int(3).to_bytes(1, byteorder="big")
+    describle_topic_partition_api = int(75).to_bytes(2, byteorder="big")
+    throttle_time_ms = 0
+    response_body = (
+        err
+        + num_api_keys
+        + api_key_bytes
+        + min_version_api_18.to_bytes(2)
+        + max_version_api_18.to_bytes(2)
+        + tag_buffer
+        + describle_topic_partition_api
+        + min_version_api_75.to_bytes(2)
+        + max_version_api_75.to_bytes(2)
+        + tag_buffer
+        + throttle_time_ms.to_bytes(4, byteorder="big")
+        + tag_buffer
+    )
+    total_len = len(response_header) + len(response_body)
+    return int(total_len).to_bytes(4, byteorder="big") + response_header + response_body
+
+def handle(client):
+    try:
         while True:
-            client_conn, addr = server.accept()
-            # receive
-            thread = threading.Thread(
-                target=handler, args=(client_conn, addr), daemon=True
-            )
-            thread.start()
+            req = client.recv(1024)
+            if not req:
+                break
+            api_key = int.from_bytes(req[4:6], byteorder="big")
+            api_version = int.from_bytes(req[6:8], byteorder="big")
+            Coreleation_ID = int.from_bytes(req[8:12], byteorder="big")
+            if api_key == 75:
+                client_id_len = int.from_bytes(req[12:14])
+                print(client_id_len)
+                if client_id_len > 0:
+                    cliend_id = req[14 : 14 + client_id_len]
+                    tagged = req[14 + client_id_len]
+                else:
+                    cliend_id = ""
+                    tagged = [14]
+                array_len_finder = 14 + client_id_len + 1
+                array_length = req[array_len_finder]
+                topic_name_length = req[array_len_finder + 1]
+                topic_name_starter = array_len_finder + 2
+                topic_name = bytes(
+                    req[
+                        topic_name_starter : topic_name_starter
+                        + (topic_name_length - 1)
+                    ]
+                )
+                cursor_length = topic_name_starter + topic_name_length + 4
+                cursor = req[cursor_length]
+                cursor_bytes = int(cursor).to_bytes(1, byteorder="big")
+                response = response_api_key_75(
+                    Coreleation_ID,
+                    cursor_bytes,
+                    array_length,
+                    topic_name_length,
+                    topic_name,
+                )
+                client.sendall(response)
+            else:
+                version = {0, 1, 2, 3, 4}
+                error_code = 0 if api_version in version else 35
+                response = create_msg(Coreleation_ID, api_key, error_code)
+                client.sendall(response)
+    except Exception as e:
+        print(f"Except Error Handling Client: {e}")
+    finally:
+        client.close()
+
+def main():
+    print("Logs from your program will appear here!")
+    
+    server = socket.create_server(("localhost", 9092), reuse_port=True)
+    
+    while True:
+        client, addr = server.accept()
+        client_Thread = threading.Thread(target=handle, args=(client,))
+        client_Thread.start()
 
 if __name__ == "__main__":
     main()
