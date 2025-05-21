8a5a4efac57151bbf5226b5a87f7bbea65a559e8
Sayan Banerjee <sayan112207@gmail.com>
2025-05-21 21:49:20 +0530

    Include DescribeTopicPartitions in APIVersions

--- app/main.py
+++ app/main.py
@@ -1,79 +1,96 @@
-import socket  # noqa: F401
-import struct
+import socket
 import threading
-from app.functions import encode_unsigned_varint, api_key
-from app.request import Request
-class Broker:
-    def __init__(self):
-        self.server = socket.create_server(("localhost", 9092), reuse_port=True)
-        self.api_versions = {
-            18: (0, 4),
-            75: (0, 0),
-        }
-        self.handlers = {
-            m._api_key: getattr(self, m.__name__)
-            for m in Broker.__dict__.values()
-            if hasattr(m, "_api_key")
-        }
-    def serve(self):
-        print("Broker listening on port 9092")
+
+NO_ERROR = 0
+
+
+class RequestValidationException(Exception):
+    code: int
+    message: str
+
+class ApiVersionInvalidException(RequestValidationException):
+    code = 35
+    message = "UNSUPPORTED_VERSION"
+
+class Request:
+    request_api_key: int
+    request_api_version: int
+    correlation_id: int
+    client_id: str | None
+    tagged_fields: list[str] | None
+
+    def __init__(self,request_api_key: int,request_api_version: int,correlation_id: int,client_id: str,):
+        self.request_api_key = request_api_key
+        self.request_api_version = request_api_version
+        self.correlation_id = correlation_id
+        self.client_id = client_id
+
+    def validate(self):
+        if self.request_api_version not in [0, 1, 2, 3, 4]:
+            raise ApiVersionInvalidException
+        
+def parse_request_length(header: bytes) -> int:
+    return int.from_bytes(header, byteorder="big", signed=True)
+
+def parse_request(request: bytes) -> Request:
+    request_api_key = int.from_bytes(request[:2], byteorder="big", signed=True)
+    request_api_version = int.from_bytes(request[2:4], byteorder="big", signed=True)
+    correlation_id = int.from_bytes(request[4:8], byteorder="big", signed=True)
+    client_id = bytes.decode(request[8:], "utf-8")
+    print("request_api_key, request_api_version, correlation_id, client_id ====== ")
+    print(request_api_key, request_api_version, correlation_id, client_id)
+    return Request(request_api_key, request_api_version, correlation_id, client_id)
+
+def create_response(request: Request) -> bytes:
+    message_bytes = request.correlation_id.to_bytes(4, byteorder="big", signed=True)
+    min_version, max_version = 0, 4
+    throttle_time_ms = 0
+    tag_buffer = b"\x00"
+    try:
+        request.validate()
+        error_bytes = NO_ERROR.to_bytes(2, byteorder="big", signed=True)
+    except RequestValidationException as ex:
+        error_bytes = ex.code.to_bytes(2, byteorder="big", signed=True)
+    message_bytes += error_bytes
+    message_bytes += int(3).to_bytes(1, byteorder="big", signed=True)
+    # if request.request_api_key == 18:
+    message_bytes += request.request_api_key.to_bytes(2, byteorder="big", signed=True)
+    message_bytes += min_version.to_bytes(2, byteorder="big", signed=True)
+    message_bytes += max_version.to_bytes(2, byteorder="big", signed=True)
+    message_bytes += tag_buffer
+    # elif request.request_api_key == 75:
+    message_bytes += int(75).to_bytes(2, byteorder="big", signed=True)
+    message_bytes += int(0).to_bytes(2, byteorder="big", signed=True)
+    message_bytes += int(0).to_bytes(2, byteorder="big", signed=True)
+    message_bytes += tag_buffer
+    message_bytes += throttle_time_ms.to_bytes(4, byteorder="big", signed=True)
+    message_bytes += tag_buffer
+    req_len = len(message_bytes).to_bytes(4, byteorder="big", signed=True)
+    response = req_len + message_bytes
+    return response
+
+def handler(client_conn, addr):
+    while True:
+        message_len = parse_request_length(client_conn.recv(4))
+        print(f"message_len {message_len}")
+        request_bytes = client_conn.recv(message_len + 8)
+        print(f"request_bytes are {request_bytes}")
+        print(int.from_bytes(request_bytes, byteorder="big", signed=True))
+        # create response
+        response = create_response(parse_request(request_bytes))
+        # send response
+        client_conn.sendall(response)
+        
+def main():
+    with socket.create_server(("localhost", 9092), reuse_port=True) as server:
+        # client_conn, addr = server.accept()
         while True:
-            con, addr = self.server.accept()
-            print(f"Accepted connection from {addr}")
-            t = threading.Thread(
-                target=self._serve_connection, args=(con,), daemon=True
+            client_conn, addr = server.accept()
+            # receive
+            thread = threading.Thread(
+                target=handler, args=(client_conn, addr), daemon=True
             )
-            t.start()
-    def _serve_connection(self, con: socket.socket):
-        with con:
-            while True:
-                try:
-                    req = Request.from_socket(con)
-                except Exception as e:
-                    print("Error handling connection:", e)
-                    break
-                try:
-                    self.handle(con, req)
-                except Exception as e:
-                    print("Handler error:", e)
-                    break
-    def handle(self, con: socket.socket, req: Request):
-        print(f"Received request: {req}")
-        if req.api_key not in self.api_versions:
-            return self._send_error(con, req.correlation_id, error_code=3)
-        min_v, max_v = self.api_versions[req.api_key]
-        if not (min_v <= req.api_version <= max_v):
-            return self._send_error(con, req.correlation_id, error_code=35)
-        handler = self.handlers.get(req.api_key)
-        if not handler:
-            return self._send_error(con, req.correlation_id, error_code=3)
-        payload = self.handlers[req.api_key](req)
-        self._send_response(con, req.correlation_id, payload)
-    def _send_response(self, conn: socket.socket, correlation_id: int, payload: bytes):
-        """
-        Send a Kafka response: [length:int32][correlation_id:uint32][payload bytefs]
-        """
-        total_length = 4 + len(payload)  # 4 bytes for correlation_id
-        header = struct.pack(">I", total_length) + struct.pack(">I", correlation_id)
-        message = header + payload
-        conn.sendall(message)
-    def _send_error(self, con: socket.socket, correlation_id: int, error_code: int):
-        """
-        Send a simple error response with only an int16 error_code in payload.
-        """
-        payload = struct.pack(">h", error_code)
-        self._send_response(con, correlation_id, payload)
-    @api_key(18)
-    def handle_api_versions(self, req: Request) -> bytes:
-        apis = list(self.api_versions.items())
-        body = struct.pack(">h", 0)
-        body += encode_unsigned_varint(len(apis) + 1)
-        for key, (min_v, max_v) in apis:
-            body += struct.pack(">hhh", key, min_v, max_v)
-            # tagged_fields for this entry (empty)
-            body += encode_unsigned_varint(0)
-        # 4) throttle_time_ms (0)
-        body += struct.pack(">i", 0)
-        # 5) final tagged_fields (empty)
-        body += encode_unsigned_varint(0)
-        return body
+            thread.start()
+
+if __name__ == "__main__":
+    main()
